b8b4c9c3cedfe0630ae592bb0105486a2395b342
2210-k <odula6346@gmail.com>
2025-09-19 22:16:36 +0500

    Add files via upload

---
+++ deepseek_javascript_20250919_482c11.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_PROJECT.firebaseapp.com",
+  databaseURL: "https://ВАШ_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "ВАШ_PROJECT_ID",
+  storageBucket: "ВАШ_PROJECT.appspot.com",
+  messagingSenderId: "ВАШ_SENDER_ID",
+  appId: "ВАШ_APP_ID"
+};
